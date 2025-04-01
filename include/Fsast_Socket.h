1ca620cac120873b7352737bf056d6819656c3de
yy <139640906+yangye1234@users.noreply.github.com>
2025-04-01 08:25:05 +0800

    添加了CMake还有一些Fsast_Socket的实现

--- include/Fsast_Socket.h
+++ include/Fsast_Socket.h
@@ -48,10 +48,7 @@ namespace FS{
 		Fsast_Socket();
 		Fsast_Socket(const Fsast_Socket&) = delete;
 		Fsast_Socket& operator=(const Fsast_Socket&) = delete;
-	protected:
-		socket_int server_socketIpv4, client_socketIpv4;//Ipv4套接字
-		socket_int server_socketIpv6, client_SocketIpv6;//Ipv6套接字
-		int port;
+		~Fsast_Socket();
 		virtual bool Initialize() = 0;//派生类的初始化
 	public:
 		//派生类的ipv4发送数据
@@ -63,13 +60,18 @@ namespace FS{
 	public:
 		static Fsast_Socket* SocketIpv4;//唯一实例化对象
 		static Fsast_Socket* SocketIpv6;//唯一实例化对象
-
+		void deletable();
+	protected:
+		socket_int server_socketIpv4, client_socketIpv4;//Ipv4套接字
+		socket_int server_socketIpv6, client_SocketIpv6;//Ipv6套接字
+		int port;
+		SYSTEM system = 0;
 	};
 	class Server_Socket : public Fsast_Socket {
 	private:
-		Server_Socket(int, IP_MODEL);//端口+模式
+		Server_Socket(int&, IP_MODEL&);//端口+模式
 	protected:
-		bool Initialize() final;//服务端的初始化
+		bool Initialize(int&,IP_MODEL&) final;//服务端的初始化
 	public:
 		//获取服务端ipv4套接字对象
 		static Fsast_Socket* GetServerSocketIpv4(int);
@@ -86,14 +88,14 @@ namespace FS{
 	};
 	class Client_Socket : public Fsast_Socket {
 	private:
-		Client_Socket(std::string&, int, IP_MODEL);//地址+端口+模式
+		Client_Socket(std::string&, int&, IP_MODEL&);//地址+端口+模式
 	protected:
-		bool Initialize() final;
+		bool Initialize(std::string&,int&,IP_MODEL&) final;
 	public:
 		//获取客户端ipv4套接字对象
 		static Fsast_Socket* GetClientSocketIpv4(std::string, int);
 		//获取客户端ipv6套接字对象
-		static Fsast_Socket* GetServerSocketIpv6(std::string, int);
+		static Fsast_Socket* GetClientSocketIpv6(std::string, int);
 		//以ipv4进行发送数据
 		virtual bool SendIpv4(std::string) override;
 		//以ipv6进行发送数据
--- src/Fsast_Socket.cpp
+++ src/Fsast_Socket.cpp
@@ -20,16 +20,50 @@
 namespace FS{
 	Fsast_Socket* Fsast_Socket::SocketIpv4 = nullptr;
 	Fsast_Socket* Fsast_Socket::SocketIpv6 = nullptr;
+	//服务端和客户端获取单例函数
 	Fsast_Socket* Server_Socket::GetServerSocketIpv4(int port){
-		if(Fsast_Socket::SocketIpv4 == nullptr){
+		if(Fsast_Socket::SocketIpv4 == nullptr && (system == 0 || system == SERVER)){
+			system = SERVER;
 			return new Server_Socket(port,IPV4);	
 		}
 		return Fsast_Socket::SocketIpv4;	
 	}
 	Fsast_Socket* Server_Socket::GetServerSocketIpv6(int port) {
-		if (Fsast_Socket::SocketIpv6 == nullptr) {
+		if (Fsast_Socket::SocketIpv6 == nullptr && (system == 0 || system == SERVER)) {
+			system = SERVER;
 			return new Server_Socket(port, IPV6);
 		}
 		return Fsast_Socket::SocketIpv6;
 	}
+	Fsast_Socket* Client_Socket::GetClientSocketIpv4(std::string address, int port) {
+		if (Fsast_Socket::SocketIpv4 == nullptr && (system == 0 || system == CLIENT)) {
+			return new Client_Socket(address, port);
+		}
+		return Fsast_Socket::SocketIpv4;
+	}
+	Fsast_Socket* Client_Socket::GetClientSocketIpv6(std::string address, int port) {
+		if (Fsast_Socket::SocketIpv6 == nullptr && (system == 0 || system == CLIENT)) {
+			return new Client_Socket(address, port);
+		}
+		return Fsast_Socket::SocketIpv6;
+	}
+	//构造函数
+	Server_Socket::Server_Socket(int& port,IP_MODEL& ipv) {
+		Initialize(port,ipv);
+	}
+	Client_Socket::Client_Socket(std::string& address, int& port, IP_MODEL& ipv) {
+		Initialize(address, port, ipv);
+	}
+	//初始化函数
+	bool Server_Socket::Initialize(int& port, IP_MODEL& ipv) {
+
+	}
+	bool Client_Socket::Initialize(std::string& address, int& port, IP_MODEL& ipv) {
+
+	}
+	//析构函数
+
+	//发送函数
+
+	//接收函数
 }
